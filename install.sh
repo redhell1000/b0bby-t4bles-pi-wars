0a6182df0b95186be6cf5f25e18d576af70a97ee
redhell1000 <dvlasits@gmail.com>
2018-11-03 00:54:26 +0000

    adding 2nd bit of xbox setup

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ sudo apt-get install python-opencv
 sudo apt-get install xboxdrv
 pip install pillow
 pip install numpy
+wget https://raw.githubusercontent.com/FRC4564/Xbox/master/xbox.py #need this for controlling xbox
